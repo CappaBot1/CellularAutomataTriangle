866b34b88960908124125e43a95c48c667458539
Casper Howse <casperhowse@gmail.com>
2026-07-14 15:19:40 +1200

    optimize a bit

--- main.js
+++ main.js
@@ -4,16 +4,18 @@ console.log(new Array(2 ** 7).fill("-").join(""));
 const downloadButton = document.getElementById("download-button");
 
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d", {
-    willReadFrequently: true // reading frequently to get the pixel data to compute the stuff and things
-});
 
-const size = 800;
+const size = 2000;
+
 const width  = size;
 const height = size;
 
 canvas.width = width;
 canvas.height = height;
+
+const ctx = canvas.getContext("2d", {
+    willReadFrequently: true // reading frequently to get the pixel data to compute the stuff and things
+});
 
 console.log(`canvas dimensions: ${width}x${height}`);
 
@@ -51,7 +53,7 @@ if (numColours*(numColours+1)/2 !== String(id).length) throw "id and numColours 
 let rulesKeys = new Array();
 
 for (let i = 1; i <= numColours; i++) rulesKeys.push(...new Array(i).fill(i));
-rulesKeys = rulesKeys.map((x, i) => [i - Math.floor((x-1)*x/2) + 1, x].join(",")); // unreadable code D:
+rulesKeys = rulesKeys.map((x, i) => [i - Math.floor((x-1)*x/2) + 1, x]); // unreadable code D:
 
 const rulesMap = new Map();
 
@@ -59,77 +61,83 @@ String(id)
     .split("")
     .map(Number)
     .forEach((rule, index) => {
-        console.log("index:", index, "rule:", rule);
-        rulesMap.set(rulesKeys[index], rule);
+        rulesMap.set(rulesKeys[index].join(","), rule);
+        rulesMap.set(rulesKeys[index].toReversed().join(","), rule);
     });
 
 for (let i = 0; i <= numColours; i++) {
-    //rulesMap.set([0, i].join(","), i);
     rulesMap.set([0, i].join(","), 0);
+    rulesMap.set([i, 0].join(","), 0);
 }
 
-rulesMap.set([0, 1].join(","), 1)
+rulesMap.set([0, 1].join(","), 1);
+rulesMap.set([1, 0].join(","), 1);
 
-console.log("rules:", rulesMap);
+//console.log("rules:", rulesMap);
 
 // fill with background colour
 ctx.fillStyle = colours[0];
 ctx.fillRect(0, 0, width, height);
 
 function computeLine(lineNo) {
+    const prevLineData = ctx.getImageData(0, lineNo*2-1, width, 1).data;
     for (
         let x = (lineNo-1)%2; // move over 1 pixel on odd lines
-        x < width-1; // don't overshoot the canvas
+        x < width-(lineNo-1)%2; // don't overshoot the canvas
         x += 2 // 2 pixels per cell
     ) {
-        //setTimeout(() => computePixel(x, lineNo), x);
-        computePixel(x, lineNo);
-    }
-}
+        // starting pixel
+        if (lineNo === 0) {
+            if (x !== Math.floor(width/2)-1+lineNo%2) continue;
 
-function computePixel(x, lineNo) {
-    if (lineNo === 0) {
-        if (x !== Math.floor(width/2)-1) return;
+            ctx.fillStyle = colours[1];
+            ctx.fillRect(x, lineNo*2, 2, 2);
 
-        ctx.fillStyle = colours[1];
+            continue;
+        }
+
+        // this is good optimization
+        // trongle check
+        // check if we're too far left
+        if (x < width/2 - lineNo - 1) continue;
+        // check if we're too far right
+        if (x > width/2 + lineNo - 1) continue;
+
+        // ~80ms at 2000 size
+        // get the data of parent cells from the image
+        const dataLeftString  = prevLineData[x*4  ] + "," +
+                                prevLineData[x*4+1] + "," +
+                                prevLineData[x*4+2] + "," +
+                                prevLineData[x*4+3];
+        const dataRightString = prevLineData[x*4+4] + "," +
+                                prevLineData[x*4+5] + "," +
+                                prevLineData[x*4+6] + "," +
+                                prevLineData[x*4+7];
+
+        // ~50ms at 2000 size
+        const left  = dataMapColour.get(dataLeftString );
+        const right = dataMapColour.get(dataRightString);
+
+        // ~70ms at 2000 size
+        const newColour = computeColour(left, right);
+
+        // ~10ms at 2000 size
+        if (newColour === undefined || newColour === null) {
+            console.error("colours from:", left, right);
+            console.error("newColour:", newColour);
+            throw "no colour :(";
+        };
+
+        // ~20ms at 2000 size
+        ctx.fillStyle = colours[newColour];
+        // ~170ms at 2000 size
         ctx.fillRect(x, lineNo*2, 2, 2);
-
-        return;
     }
-
-    // check if we're too far left
-    if (x < width/2 - lineNo - 1) return;
-    // check if we're too far right
-    if (x > width/2 + lineNo - 1) return;
-
-    // get the data of parent cells from the image
-    const dataLeft  = ctx.getImageData(x  , lineNo*2-1, 1, 1);
-    const dataRight = ctx.getImageData(x+1, lineNo*2-1, 1, 1);
-
-    // turn the data into string so I can do map lookup
-    const dataLeftString  = Array(dataLeft .data).join(",");
-    const dataRightString = Array(dataRight.data).join(",");
-
-    const left  = dataMapColour.get(dataLeftString );
-    const right = dataMapColour.get(dataRightString);
-
-    const newColour = computeColour(left, right);
-
-    if (newColour === undefined || newColour === null) {
-        console.error("colours from:", left, right);
-        console.error("newColour:", newColour);
-        throw "no colour :(";
-    };
-
-    ctx.fillStyle = colours[newColour];
-    ctx.fillRect(x, lineNo*2, 2, 2);
 }
 
-// time complexity is not coming from here
+// ~20ms at 2000 size
 function computeColour(left, right) {
-    const ruleName = (left < right ? [left, right] : [right, left])// flip around the colours so left is smaller than right
-        .join(","); // stringify it for map lookup
-    
+    const ruleName = left + "," + right; // stringify it for map lookup
     return rulesMap.get(ruleName);
 }
 
@@ -140,18 +148,15 @@ downloadButton.innerText = "(not ready) " + downloadButton.innerText;
 const startTime = performance.now();
 console.log("computing started at:", startTime);
 for (let i = 0; i < height/2; i++) {
-    setTimeout(() => {computeLine(i)}, i);
-    //computeLine(i);
+    computeLine(i);
 }
 
-setTimeout(() => {
-    const endTime = performance.now();
-    console.log("computing ended at:", endTime);
+const endTime = performance.now();
+console.log("computing ended at:", endTime);
 
-    console.log(`total time: ${((endTime-startTime)/1000).toFixed(4)}s`);
+console.log(`total time: ${((endTime-startTime)/1000).toFixed(4)}s`);
 
-    // set up button to download the image
-    const img = canvas.toDataURL("image/png");
-    downloadButton.href = img;
-    downloadButton.innerText = downloadButton.innerText.replace(notReadyText, "");
-}, height/2+1);
+// set up button to download the image
+const img = canvas.toDataURL("image/png");
+downloadButton.href = img;
+downloadButton.innerText = downloadButton.innerText.replace(notReadyText, "");
